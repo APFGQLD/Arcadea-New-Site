dfdbb80193c4b40800ab497fa6c05cd93ae67b8c
APFGQLD <apfgqld@gmail.com>
2026-02-04 11:31:14 +1000

    feat: Initialize React application with routing, theming, and internationalization support.

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ThemeProvider } from './context/ThemeContext';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './i18n';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
-      <HashRouter>
+      <BrowserRouter>
         <Suspense fallback={<div style={{ background: '#0a0a0a', height: '100vh' }} />}>
           <App />
         </Suspense>
-      </HashRouter>
+      </BrowserRouter>
     </ThemeProvider>
   </StrictMode>,
 )
